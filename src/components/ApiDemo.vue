2650468f4d735e873a1a0be88a252fa8448b1a8b
Mi&Jack <mijackstudio@gmail.com>
2019-12-13 22:25:01 +0800

    update axios

---
+++ src/components/ApiDemo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        {{ info }}
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        name: "ApiDemo",
+        data() {
+            return {
+                info: null
+            }
+        },
+        mounted() {
+            axios.get("http://localhost:8080/api/users/list", {
+                // headers: {'X-Custom-Header': 'foobar'}
+            })
+                .then(response => (this.info = response))
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
