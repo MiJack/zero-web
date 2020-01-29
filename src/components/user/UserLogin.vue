76fa8d81a2e8db7b12926af63ab66ee5ae3c648a
Mi&Jack <mijackstudio@gmail.com>
2020-01-29 16:14:31 +0800

    update UserLogin

--- src/components/user/UserLogin.vue
+++ src/components/user/UserLogin.vue
@@ -1,35 +1,55 @@
 <template>
-    <div>
-        <form @submit.prevent="submit">
-            邮箱： <input name="user" v-model="user_email"> <br/>
-            密码： <input name="password" type="password" v-model="userPassword"> <br/>
-            <button type="submit">登录</button>
-        </form>
-        <router-link to="/user/register">注册</router-link>
+    <div id="components-form-demo-normal-login">
+
+        <a-form id="components-form-demo-normal-login" class="login-form" @submit="handleSubmit">
+            <a-form-item>
+                <a-input v-model="userEmail" placeholder="User Email">
+                    <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)"/>
+                </a-input>
+            </a-form-item>
+            <a-form-item>
+                <a-input v-model="userPassword" type="password" placeholder="Password">
+                    <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)"/>
+                </a-input>
+            </a-form-item>
+            <a-form-item>
+                <a-checkbox v-model="remember">Remember me</a-checkbox>
+                <a class="login-form-forgot" href="/user/forgotPwd">
+                    Forgot password
+                </a>
+                <a-button type="primary" html-type="submit" class="login-form-button">
+                    Log in
+                </a-button>
+                Or
+                <router-link to="/user/register">register now!</router-link>
+
+            </a-form-item>
+        </a-form>
     </div>
-
-
 </template>
 
 <script>
 
     import axios from 'axios'
-    import {csrf_head,api_head} from "@/static/constans"
+    import {csrf_head, api_head} from "@/static/constans"
 
     export default {
         name: "UserLogin",
         data: function () {
             return {
-                user_email: null,
+                userEmail: null,
+                remember: false,
                 userPassword: null
             }
         },
         methods: {
-            submit: function () {
+            handleSubmit(e) {
+                e.preventDefault();
                 axios.post("/api/user/login", {
                     loginType: 1,
-                    identifiableValue: this.user_email,
-                    password: this.userPassword
+                    identifiableValue: this.userEmail,
+                    password: this.userPassword,
+                    remember: this.remember
                 }, {
                     headers: {
                         "X-XSRF-TOKEN": this.$cookies.get(csrf_head)
@@ -71,4 +91,19 @@
 
 <style scoped>
 
+    #components-form-demo-normal-login {
+        align-content: center;
+    }
+
+    #components-form-demo-normal-login .login-form {
+        max-width: 300px;
+    }
+
+    #components-form-demo-normal-login .login-form-forgot {
+        float: right;
+    }
+
+    #components-form-demo-normal-login .login-form-button {
+        width: 100%;
+    }
 </style>
